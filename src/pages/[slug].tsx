96f817906012af9cb8bf6bdedf27c663c3d64d08
Hugo Romerito <77184434+hugoromerito@users.noreply.github.com>
2023-02-22 17:00:23 -0300

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -98,7 +98,7 @@ export default function Post({ slug }) {
         </div>
         
         <div 
-          className="mt-6 prose dark:prose-invert prose-a:no-underline hover:prose-a:underline prose-purble  prose-lg mx-auto ease-in duration-300" 
+          className="mt-6 prose dark:prose-invert prose-a:no-underline hover:prose-a:underline prose-a:text-purble-500  prose-lg mx-auto ease-in duration-300" 
           dangerouslySetInnerHTML={{ __html: post.content.html }} 
         />
       </div>
